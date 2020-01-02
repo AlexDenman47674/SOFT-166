0d88e9c2396c6e9d272d625f8ffead5a68d738a1
AlexDenman47674 <57488236+AlexDenman47674@users.noreply.github.com>
2020-01-02 12:08:21 +0000

    Game file now includes all needed functions (formatting required)

--- GameFile.js
+++ GameFile.js
@@ -1,15 +1,66 @@
 var Sequence = [];
 var i;
+var List = [];
+var Correct=true;
+var UserNum =[];
 
-function RestartGame()
-{
-    Sequence=[];
-    for (i = 1; i <=6; i++) {
-        togglelight(i);
-    }
-    alert("Game restarted")
-}
 function StartGame()
 {
     alert("Game starting...")
+	List = [];
+	UserNum=[];
+	AddList();
+	Correct=true;
+}
+
+function RestartGame()
+{
+	alert("Restarting game...")
+	List=[];
+	UserNum=[];
+	AddList();
+	Correct=true;
+}
+
+function AddList()
+{
+	var List =[];
+	List.push(Math.round(Math.random() * 5))
+}
+
+
+function CompareList()
+{
+	var i;
+	for (i = 0; i < List.length; i++) {
+		if Correct=true;
+	{
+		if List[i]=UserNum[i]
+		{
+			Correct=true;
+			
+		}
+	
+		else
+		{
+			Correct=false;
+		}
+	}
+	}
+	if Correct=true
+	{
+		alert("Correct! Starting next round")
+		UserNum=[];
+		AddList();
+	}
+	else
+	{
+		alert("Incorrect! Restart to try again")
+	}
+
+}
+
+function GetUserNum(Input)
+{
+	UserNum.push(Input)
 }
