d5a98b2dd6f89570dfcc9941db0780c180fd0a8d
AlexDenman47674 <57488236+AlexDenman47674@users.noreply.github.com>
2020-01-08 12:13:00 +0000

    Update GameFile.js

--- GameFile.js
+++ GameFile.js
@@ -3,64 +3,58 @@ var i;
 var List = [];
 var Correct=true;
 var UserNum =[];
+var Counter = 0;
 
 function StartGame()
 {
     alert("Game starting...")
+	Counter=0;
 	List = [];
 	UserNum=[];
-	AddList();
 	Correct=true;
-}
-
-function RestartGame()
-{
-	alert("Restarting game...")
-	List=[];
-	UserNum=[];
 	AddList();
-	Correct=true;
 }
 
 function AddList()
 {
-	var List =[];
-	List.push(Math.round(Math.random() * 5))
+	List.push(Math.round(Math.random() * 5)+1)
+	RunGame();
 }
 
 
-function CompareList()
-{
-	var i;
-	for (i = 0; i < List.length; i++) {
-		if Correct=true;
-	{
-		if List[i]=UserNum[i]
-		{
-			Correct=true;
-			
-		}
-	
-		else
-		{
-			Correct=false;
-		}
-	}
-	}
-	if Correct=true
-	{
-		alert("Correct! Starting next round")
-		UserNum=[];
-		AddList();
-	}
-	else
-	{
-		alert("Incorrect! Restart to try again")
-	}
 
-}
 
 function GetUserNum(Input)
 {
-	UserNum.push(Input)
+	if (Input === List[Counter]) {
+		Correct=true;
+		if (Counter === List.length-1){
+			AddList();
+		} else {
+			Counter+=1;
+		}
+	} else {
+		Correct=false;
+		alert("Game over")
+	}
 }
+	
+function RunGame()
+{
+	alert("The sequence is ")
+	alert(List)
+	//Then the user will be prompted to repeat the sequence
+	alert("Now repeat the sequence")
+	//The on click event will run to store the users inputs
+	Counter=0;
+}
+
+function Colour(ColInput)
+{
+	$(document.getElementById(ColInput)).removeClass("bg-light").addClass("bg-danger");
+}
+function ColourOff(ColInput)
+{
+	$(document.getElementById(ColInput)).removeClass("bg-danger").addClass("bg-light");	
+}
+
